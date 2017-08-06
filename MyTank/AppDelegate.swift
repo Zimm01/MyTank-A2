310a7ba2ae536d2ad21fbf988ca0d5aaa26a4638
Daniel Zimmerman <Zimmo@daniels-imac.local>
2017-08-06 23:15:49 +1000

    Database Remodelling - MVVM Restructuring

--- MyTank/AppDelegate.swift
+++ MyTank/AppDelegate.swift
@@ -9,98 +9,180 @@
 import UIKit
 import CoreData
 
+enum UserKeys : String
+{
+    case preLoaded = "isPreloaded"
+    
+    case vehicleFileSize = "vehicFileSize"
+    
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    // The UI window placeholder
     var window: UIWindow?
+    
+    // User defaults constant
+    let defaults = UserDefaults.standard
+    
+    let JSONFile = Bundle.main.url(forResource: "VehicleList", withExtension: "json")
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        preloadData()
+        
+        // First, check the CURRENT JSON data has not previously been loaded into the database
+        // if it has we can skip preloading this information
+        if !startupLoadCheck()
+        {
+            // Our tests have failed, so we will wipe and re-load the vehicle database
+            wipeVehicleData()
+            preloadVehicleData()
+            // TODO function that removes user vehicle!
+        }
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
+    // This function tests to see if the most recent JSON datafile has been loaded into CoreData and returns true only if this is the case.
+    func startupLoadCheck() -> Bool
+    {
+        // First, if the database is NOT preloaded, or the size of the file previously used is non existant return false straight away
+        if !defaults.bool(forKey: UserKeys.preLoaded.rawValue) || defaults.double(forKey: UserKeys.vehicleFileSize.rawValue) == 0{
+            return false
+        }
+        
+        
+        // If the database has been preloaded, we want to check the size of the file used to do so
+        // If it is a different size, it will contain different data
+        if let file = JSONFile
+        {
+            do{
+                let filesize = try FileManager.default.attributesOfItem(atPath: file.path)[FileAttributeKey.size] as! Double
+                
+                if filesize != defaults.double(forKey: UserKeys.vehicleFileSize.rawValue){
+                    return false
+                }
+            }
+            catch{
+                print(error.localizedDescription)
+            }
+        }
+        return true
+    }
+    
+    
+    func applicationWillResignActive(_ application: UIApplication)
+    {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
     
-    func applicationDidEnterBackground(_ application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication)
+    {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
-    func applicationWillEnterForeground(_ application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication)
+    {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
     
-    func applicationDidBecomeActive(_ application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication)
+    {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
-    func applicationWillTerminate(_ application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication)
+    {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
        // self.saveContext()
     }
   
-    func preloadData()
+    // This funtion takes a Vehicle JSON file and adds it to CoreData
+    func preloadVehicleData()
     {
         // Open the object context and prepare a path to insert data
-        let managedObjectContex = NSPersistentContainer(name: "Model").viewContext
+        let managedObjectContex = persistentContainer.viewContext
         let vehicleEntityDesc = NSEntityDescription.entity(forEntityName: "Vehicle2", in: managedObjectContex)
-        let vehicle = Vehicle2(entity: vehicleEntityDesc! , insertInto: managedObjectContex)
         
+        // Our dynamic ID will be used as a "key" for DB interaction
+        var dynamicId:Int32 = 0
         
-        var vehicleList = [String:Any]()
-        
+        // We will attempt to serialize the JSON file and then add it to the corresponding Data Entity
         do
         {
-            if let file = Bundle.main.url(forResource: "VehicleList", withExtension: "json")
+            if let file = JSONFile
             {
                 let data = try Data(contentsOf: file)
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                let parsedData = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]
                 
-                if let object = json as? [String:Any]
+                // Parse the file as dictionary of objects
+                if let serializedData = parsedData?["vehicles"] as? [[String:Any]]
                 {
-                    for value in object
+                    
+                    // FOR EACH entry in the file, we will parse it into a Vehicle Entity
+                    for dictionary in serializedData
                     {
-                        print(value.value)
-                        print("BREAK")
+                        let vehicleData = Vehicle2(entity: vehicleEntityDesc! , insertInto: managedObjectContex)
+                        
+                        vehicleData.setValue(dynamicId, forKey: "id")
+                        vehicleData.setValue(dictionary["make"], forKey: "make")
+                        vehicleData.setValue(dictionary["model"], forKey: "model")
+                        vehicleData.setValue(dictionary["variant"], forKey: "variant")
+                        vehicleData.setValue(dictionary["type"], forKey: "type")
+                        vehicleData.setValue(dictionary["yearStart"], forKey: "yearStart")
+                        vehicleData.setValue(dictionary["yearEnd"], forKey: "yearEnd")
+                        vehicleData.setValue(dictionary["consumptionLitres"], forKey: "consumptionLitres")
+                        
+                        dynamicId += 1
                     }
+
+                    // Finally we will set the file size and the "PreLoaded" flag into the default user
+                    // data for next startup!
+                    let filesize = try FileManager.default.attributesOfItem(atPath: (JSONFile?.path)!)[FileAttributeKey.size] as! Double
+                    print("0" + String(filesize))
+                    
+                    // Commit the values here!
+                    UserDefaults.standard.setValue(true, forKey: UserKeys.preLoaded.rawValue)
+                    UserDefaults.standard.setValue(filesize, forKey: UserKeys.vehicleFileSize.rawValue)
                 }
                 else
                 {
-                    print("Invalid")
+                    print("Invalid File!")
                 }
             }
             else{
                 print("No file")
             }
-           // let data = try! Data(contentsOf: URL(fileURLWithPath: path))
-                //let jsonObj = JSON(data: data)
-                    
-                    
-          //  vehicleList = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
-
             
+            // Save the context here, the parsing is complete!
+            try managedObjectContex.save()
         }
         catch
         {
-            print("oop")
+            print(error.localizedDescription + " AT ROW: " + String(dynamicId))
         }
-        // The location of our JSON file
-        //let jsonData : NSData = try! NSData(contentsOfFile: "/VehicleList.json")
-        
-        //var vehicleList = [[String:Any]]()
-        
-        //vehicleList = try! JSONSerialization.jsonObject(with: "VehicleList.json", options: []) as! [[String:Any]];
-        
-        print(vehicleList.count)
     }
     
-    
+    // This function deletes all entries under our Vehicle Entity in the database
+    func wipeVehicleData()
+    {
+        let fetchRequest = NSFetchRequest<Vehicle2>(entityName: "Vehicle2")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest as! NSFetchRequest<NSFetchRequestResult>)
+        
+        let db = persistentContainer.viewContext
+        do{
+            try db.execute(deleteRequest)
+            UserDefaults.standard.setValue(false, forKey: UserKeys.preLoaded.rawValue)
+        }
+        catch
+        {
+            print(error.localizedDescription)
+        }
+    }
     
     // MARK: - Core Data stack
     
--- MyTank/VehicleViewController.swift
+++ MyTank/VehicleViewController.swift
@@ -7,33 +7,123 @@
 //
 
 import UIKit
+import CoreData
 
 class VehicleViewController: UITableViewController
 {
     //let context = (UIApplication.shared.delegate as! AppDelegate) .persistentContainer.viewContext
-    var vehicles: [Vehicle2] = []
+    //private var vehicles: [Vehicle2] = []
     
+    // Our persistant container from the CoreData Model
+    private let persistentContainer = NSPersistentContainer(name: "Model")
+    
+    // This variable is used to fetch data from the Data Model
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<Vehicle2> = {
+        // Create Fetch Request
+        let fetchRequest: NSFetchRequest<Vehicle2> = Vehicle2.fetchRequest()
+        
+        // Configure Fetch Request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+        
+        // Create Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        fetchedResultsController.delegate = self
+        
+        return fetchedResultsController
+    }()
+    
+    // ON load of this view
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        //tableView.delegate = self
+        //tableView.dataSource = self
+        
+        persistentContainer.loadPersistentStores { (persistentStoreDescription, error) in
+            if let error = error {
+                print("Unable to Load Persistent Store")
+                print("\(error), \(error.localizedDescription)")
+            }
+            else {
+                self.setupView()
+                
+                do {
+                    try self.fetchedResultsController.performFetch()
+                } catch {
+                    let fetchError = error as NSError
+                    print("Unable to Perform Fetch Request")
+                    print("\(fetchError), \(fetchError.localizedDescription)")
+                }
+                self.updateView()
+            }
+        }
     }
     
+    
     override func viewWillAppear(_ animated: Bool) {
-        //getData()
+
         tableView.reloadData()
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let vehicle = vehicles[indexPath.row]
+    //  This function is used to determine the table attributes as well as the number of rows to be used, based on the number of vehicles in the library
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // Set the Background Colour
+        self.tableView.backgroundColor = UIColor(red: 0.47, green: 0.47, blue: 0.47, alpha: 1)
+        
+        
+        if let vehicles = fetchedResultsController.fetchedObjects{
+         
+            print(vehicles.count)
+            return vehicles.count
+        }
+        return 0
+    }
     
+    
+    
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+    
+        // Fetch Vehicle
+        let vehicle = fetchedResultsController.object(at: indexPath)
+    
+        // Setup cell
+        cell.textLabel?.text = vehicle.make
+        cell.detailTextLabel?.text = String(vehicle.consumptionLitres)
+
         return cell
+    }
+    
+    private func updateView(){
+        
+        var hasQuotes = false
+        
+        if let quotes = fetchedResultsController.fetchedObjects {
+            hasQuotes = quotes.count > 0
+        }
+        
+        tableView.isHidden = !hasQuotes
+        
+    }
+    
+    private func setupView(){
+        
+        updateView()
+    }
+    
+    //  Replaces the Back item with the word "BACK" in the preceding view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let backItem = UIBarButtonItem()
+        backItem.title = "Back"
+        navigationItem.backBarButtonItem = backItem
     }
 }
 
-    
-    
+extension UITableViewController: NSFetchedResultsControllerDelegate
+{
+}
     /*
     // Our Current Vehicle Data Model
     var vehicleData: VehicleData! = VehicleData()
