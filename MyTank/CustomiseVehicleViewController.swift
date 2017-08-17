58029e7007f1a1faaa4af124683ca259358ef8ef
Daniel Zimmerman <Zimmo@daniels-imac.local>
2017-08-18 00:48:02 +1000

    customise Vehicle

--- MyTank/CustomiseVehicleViewController.swift
+++ MyTank/CustomiseVehicleViewController.swift
@@ -8,31 +8,55 @@
 import UIKit
 import CoreData
 
-// Contains Common Functions for all Vehicle Table View Components of MYTank!!
+// Contains Functions to handle the vehicle customisation delegation and db commital controls
 class CustomiseVehicleViewController: UIViewController, CommitDetailsToDBDelegate
 {
+    // Our View Model for this moduel
+    internal let customiserViewModel = CustomiseVehicleViewModel()
+    
+    // The container which handles vehicle customisation
     var customSelectorContainer: CustomSelectorModuleViewController?
     
+    // Button to segue to the next view, after commiting a vehicle to the Database
+    @IBOutlet weak var segueButton: UIButton!
+    
+    // On Load..
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        customSelectorContainer?.commitToDBSignal(sendCommit: true)
-        print("hi")
-        
     }
     
+    // Setup Segue, either for our Delegate or just in general
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        print("try")
-        if segue.identifier == "CustomiseVehicleSegue"
+        if segue.identifier == "VehicleCustomiserModule"
         {
             customSelectorContainer = segue.destination as? CustomSelectorModuleViewController
             customSelectorContainer!.commitDetailsDelegate = self
         }
+        else
+        {
+            let backItem = UIBarButtonItem()
+            backItem.title = "Back"
+            navigationItem.backBarButtonItem = backItem
+        }
     }
-
-    func commitToDBSignal(sendCommit: Bool)
+    
+    // This action is executed on button push, it will request the series and variant from the container view and then pass those values to the view model
+    @IBAction func commitVehicleAndTransition(button: UIButton)
     {
-        print("huh")
+        if customiserViewModel.commitSeriesVairantToDB(tuple: customSelectorContainer!.retrieveVehicleSpecifics())
+        {
+            self.performSegue(withIdentifier: "LocationSelectionSegue", sender: self)
+        }
+        else
+        {
+            _ = self.navigationController?.popToRootViewController(animated: false)
+        }
+    }
+    
+    // Placeholder so the class conforms with the Delegate Protocol
+    internal func retrieveVehicleSpecifics() -> (series: String, variant: String) {
+        return ("Error","Error")
     }
 }
